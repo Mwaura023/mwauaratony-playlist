f343c97ec5a593372f7f376603ed38ee2c9a854b
Mwaura023 <mwaurasony023@gmail.com>
2025-09-01 02:34:04 +0300

    password is set

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+if os.environ.get('RENDER'):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@mwauaratony.com', 'admin123')
